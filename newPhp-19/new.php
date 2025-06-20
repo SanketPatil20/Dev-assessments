0eb578274f88dc16d9319d3059a12f009cf3b183
SanketPatil20 <sanketvp01@gmail.com>
2025-06-20 23:29:16 +0530

    new to php

---
+++ newPhp-19/new.php
@@ -0,0 +1,33 @@
+<?php
+// PHP file
+
+// single-line comment
+
+/*
+ multi
+ line 
+ comment.
+ 
+*/
+
+echo "Hello, this is my first PHP program!<br>";
+
+print "I am learning how to use print in PHP.<br>";
+
+
+$name = "Sanket"; 
+$age = 20; 
+$grade = 85.5; 
+$isEnrolled = true; 
+
+
+echo "My name is $name<br>";
+echo "I am $age years old<br>";
+echo "My grade is $grade<br>";
+
+
+
+echo "Enrolled in course: ";
+echo $isEnrolled; 
+
+?>
