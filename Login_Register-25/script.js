1a2df5faa9fb87f3389aa31bb73e81fa541c59bd
SanketPatil20 <sanketvp01@gmail.com>
2025-07-03 22:40:24 +0530

    newly added

--- Login_Register-25/script.js
+++ Login_Register-25/script.js
@@ -5,11 +5,21 @@ const loginBtnModal = document.querySelector('.login-btn-modal');
 const closeBtnModal = document.querySelector('.close-btn-modal');
 const profileBox = document.querySelector('.profile-box');
 const avatarCircle = document.querySelector('.avatar-circle');
+const alertBox = document.querySelector('.alert-box');
 
 registerLink.addEventListener('click', () => authModal.classList.add('slide'));
 loginLink.addEventListener('click', () => authModal.classList.remove('slide'));
 
-loginBtnModal.addEventListener('click', () => authModal.classList.add('show'));
+if (loginBtnModal) loginBtnModal.addEventListener('click', () => authModal.classList.add('show'));
 closeBtnModal.addEventListener('click', () => authModal.classList.remove('show', 'slide'));
 
-avatarCircle.addEventListener('click', () => profileBox.classList.toggle('show'));
+if (avatarCircle) avatarCircle.addEventListener('click', () => profileBox.classList.toggle('show'));
+
+if(alertBox){
+
+    setTimeout(() => alertBox.classList.add('show'), 50);
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+        setTimeout(() => alertBox.remove(), 1000);
+    }, 6000);
+}
